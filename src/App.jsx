f04f08385680edecb325390ab9971c826a04a046
Ckott <diegoangeles130703@gmail.com>
2024-12-19 23:19:46 -0500

    Agregar configuración de API y rutas, implementar hook de fetch, y actualizar componentes para utilizar nuevos servicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/home"
 import { Users } from "./pages/users"
+import { routes } from "./routes"
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/Usuarios" element={<Users/>}/>
-                    <Route path="/" element={<Home/>}></Route>
+                    <Route path={routes.home} element={<Home/>}/>
+                    <Route path={routes.users} element={<Users/>}/>
                 </Routes>
             </BrowserRouter>
         </>
